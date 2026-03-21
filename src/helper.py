e377cd27c7cad4ab1c30a8feae3d39784b1fd7cc
Patrick Hammer <patham9@gmail.com>
2026-03-21 11:00:55 +0100

    Helperpy for parens blance

---
+++ src/helper.py
@@ -0,0 +1,10 @@
+def balance_parentheses(s):
+    s = s.strip()
+    left = 0
+    while left < len(s) and s[left] == '(':
+        left += 1
+    right = 0
+    while right < len(s) and s[len(s) - 1 - right] == ')':
+        right += 1
+    core = s[left:len(s) - right if right else len(s)].strip()
+    return f"(({core}))"
